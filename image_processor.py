8b44e7483532c56c23a10792a14568e81909610e
Xavier M.W <69274109+XavierFMW@users.noreply.github.com>
2022-07-07 19:23:07 -0600

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -3,12 +3,11 @@ from collections import OrderedDict
 
 class ImageProcessor:
 
-    characters = OrderedDict()
+    def __init__(self):
+        self.initialize_char_set()
 
-    def __init__(self, image, pixels_per_char):
-        self.image = image
-        self.pixels_per_char = pixels_per_char
-
+    def initialize_char_set(self):
+        self.characters = OrderedDict()
         self.characters[25] = " "
         self.characters[50] = "."
         self.characters[75] = "^"
@@ -20,57 +19,39 @@ class ImageProcessor:
         self.characters[225] = "#"
         self.characters[250] = "█"
 
-    def image_to_ascii(self):
-        self.check_for_invalid_image()
+    def image_to_ascii(self, image, pixels_per_char):
+        self.validate_image(image)
 
         output = ""
-        size = self.image.size
+        size = image.size
 
         y = 0
         while y < size[1]:
 
             x = 0
             while x < size[0]:
-                luminosity = self.get_luminosity(x, y)
-                new_char = self.get_new_character(luminosity)
-                output += new_char
-                x += self.pixels_per_char
+                luminosity = self.get_luminosity(image, x, y)
+                output += self.get_char_of_luminosity(luminosity)
+                x += pixels_per_char
 
             output += "\n"
-            y += self.pixels_per_char * 2
+            y += pixels_per_char * 2
 
         return output
 
-    @staticmethod
-    def get_new_character(luminosity):
-        if luminosity <= 25:
-            return " "
-        elif luminosity <= 50:
-            return "."
-        elif luminosity <= 75:
-            return "^"
-        elif luminosity <= 100:
-            return ";"
-        elif luminosity <= 125:
-            return "/"
-        elif luminosity <= 150:
-            return "4"
-        elif luminosity <= 175:
-            return "X"
-        elif luminosity <= 150:
-            return "%"
-        elif luminosity <= 225:
-            return "#"
-        else:
-            return "█"
+    def get_char_of_luminosity(self, luminosity):
+        for char_luminosity, character in self.characters.items():
+            if luminosity <= char_luminosity:
+                return character
 
-    def get_luminosity(self, x, y):
-        self.check_for_invalid_image()
-        pixel = self.image.load()
-        rgb_values = pixel[x, y][:3]
-        luminosity = sum(rgb_values) // 3
+    @staticmethod
+    def get_luminosity(image, x, y):
+        pixels = image.load()
+        pixel = pixels[x, y][:3]
+        luminosity = sum(pixel) // 3
         return luminosity
 
-    def check_for_invalid_image(self):
-        if not self.image:
+    @staticmethod
+    def validate_image(image):
+        if not image:
             raise Exception("attempted to process unset image")
